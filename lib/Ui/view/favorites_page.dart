d8bf8c051f460e6e328bc27515bd71e6f940d88a
Juan José <jujopere@bancolombia.com.co>
2022-08-23 07:44:44 -0500

    se reorganiza proyecto con carpeta Ui

---
+++ lib/Ui/view/favorites_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:zemoga_test/domain/models/posts.dart';
+
+import '../view_model/favorites_view_model.dart';
+
+class FavoritesPage extends StatelessWidget {
+  const FavoritesPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final favoritesProviderList =
+        Provider.of<FavoritesViewModel>(context, listen: true).favoritesList;
+
+    return favoritesProviderList.isNotEmpty
+        ? ListView.separated(
+            shrinkWrap: true,
+            itemBuilder: (context, index) {
+              Posts post = favoritesProviderList[index];
+              return Card(
+                child: ListTile(
+                  title: Text(post.title!),
+                ),
+              );
+            },
+            separatorBuilder: (context, index) => Container(
+              height: 10,
+            ),
+            itemCount: favoritesProviderList.length,
+          )
+        : const Center(child: Text("No data"));
+  }
+}
